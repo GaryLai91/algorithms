13e6d3582fad1998198f477ddef2fb1a0e624ccf
Gary Lai Yung Hui <garylai@Garys-MacBook-Pro.local>
2020-11-20 22:49:47 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="algorithms",  # Replace with your own username
+    version="0.0.1",
+    description="Algorithms for the stanford algorithms course",
+    packages=setuptools.find_packages(),
+    # python_requires=">=3.6",
+)
