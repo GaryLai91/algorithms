d1b4fcd20fbcefaa6b6c270d72e22bbc301adf91
garylai <garylai91@gmail.com>
2020-12-11 05:21:02 -0600

    refactor kosaraju.py

--- algorithms/kosaraju.py
+++ algorithms/kosaraju.py
@@ -1,75 +1,105 @@
-from topological_sort import TopoSort
 from collections import Counter
 
-num_scc = 0
-scc = {}
-explored = []
+
+class Kosaraju():
+    def __init__(self, graph):
+        self.graph = graph
+        self.num_scc = 0
+        self.scc = {}
+        self.explored = []
+        self.current_label = len(self.graph)
+        self.topo_explored = []
+        self.topo_order = {}
+
+    def _reverse_graph(self, G):
+        """
+        Reverse a directed graph represented as an adjacency list
+        """
+        g_rev = {k: [] for k in G.keys()}
+        for k in G.keys():
+            for v in G[k]:
+                g_rev[v].append(k)
+        return g_rev
+
+    def _format_scc(self, scc):
+        """
+        Helper function to get top 5 scc sizes for 
+        homework solutions and test case purposes
+        """
+        print(scc)
+        count = Counter(scc.values())
+        values = sorted(list(count.values()), reverse=True)
+        while len(values) < 5:
+            values.append(0)
+        return values[:5]
+
+    def TopoSort(self, G):
+        """
+        Topological Sort of a DAG
+        G: a graph represented as an adjacency list
+        """
+        vertices = list(G.keys())
+        for v in vertices:
+            if v not in self.topo_explored:
+                self.DFS_Topo(G, v)
+        order_ascending = {
+            k: v
+            for k, v in sorted(self.topo_order.items(),
+                               key=lambda item: item[1])
+        }
+        return order_ascending
+
+    def DFS_Topo(self, G, s):
+        """
+        Explore the graph using DFS and mark nodes as explored
+        and assign a topological ordering value
+        """
+        self.topo_explored.append(s)
+        for v in G[s]:
+            if v not in self.topo_explored:
+                self.DFS_Topo(G, v)
+
+        self.topo_order[s] = self.current_label
+        self.current_label -= 1
+
+    def DFS_SCC(self, G, s):
+        """
+        Evert vertex reachable from s is marked as explored 
+        and has an assigned scc-value
+        """
+        self.explored.append(s)
+        self.scc[s] = self.num_scc
+        for v in self.graph[s]:
+            if v not in self.explored:
+                self.DFS_SCC(G, v)
+
+    def get_scc(self):
+        """
+        Find strongly connected components.
+        """
+        g_rev = self._reverse_graph(self.graph)
+        g_rev_order = self.TopoSort(g_rev)
+        for v in g_rev_order.keys():
+            if v not in self.explored:
+                self.num_scc += 1
+                self.DFS_SCC(self.graph, v)
+        return self._format_scc(self.scc)
 
 
-def reverse_graph(G):
-    """
-    Reverse a directed graph represented as an adjacency list
-    """
-    g_rev = {k: [] for k in G.keys()}
-    for k in G.keys():
-        for v in G[k]:
-            g_rev[v].append(k)
-    return g_rev
-
-
-def format_scc(scc):
-    """
-    Helper function to get top 5 scc sizes for 
-    homework solutions and test case purposes
-    """
-    count = Counter(scc.values())
-    values = sorted(list(count.values()), reverse=True)
-    while len(values) < 5:
-        values.append(0)
-    return values
-
-
-def DFS_SCC(G, s):
-    """
-    Evert vertex reachable from s is marked as explored 
-    and has an assigned scc-value
-    """
-    global num_scc, scc, explored
-    explored.append(s)
-    scc[s] = num_scc
-    for v in G[s]:
-        if v not in explored:
-            DFS_SCC(G, v)
-
-
-def Kosaraju(G):
-    """
-    Find strongly connected components.
-    """
-    global num_scc, scc, explored
-    g_rev = reverse_graph(G)
-    g_rev_order = TopoSort(g_rev)
-    for v in g_rev_order.keys():
-        if v not in explored:
-            num_scc += 1
-            DFS_SCC(G, v)
-    return format_scc(scc)
-
-
-# if __name__ == "__main__":
-#     G = {
-#         1: [2],
-#         2: [3, 4, 5],
-#         3: [6],
-#         4: [5, 7],
-#         5: [2, 6, 7],
-#         6: [3, 8],
-#         7: [8, 10],
-#         8: [7],
-#         9: [7],
-#         10: [9, 11],
-#         11: [12],
-#         12: [10]
-#     }
-#     scc = Kosaraju(G)
-#     print(scc)
+if __name__ == "__main__":
+    G = {
+        1: [2],
+        2: [3, 4, 5],
+        3: [6],
+        4: [5, 7],
+        5: [2, 6, 7],
+        6: [3, 8],
+        7: [8, 10],
+        8: [7],
+        9: [7],
+        10: [9, 11],
+        11: [12],
+        12: [10]
+    }
+    kosaraju = Kosaraju(G)
+    print(kosaraju.get_scc())
